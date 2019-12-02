af7bc6f378b3644faabacc83bd0d50fc836e9ba6
DominikKunze <dominik.kunze99@web.de>
2019-12-02 14:02:22 +0100

    Add args

--- src/com/unitedinternet/Main.java
+++ src/com/unitedinternet/Main.java
@@ -6,7 +6,26 @@ import com.unitedinternet.Libary.PortChecker;
 public class Main {
 
     public static void main(String[] args) {
-        Gui gui = new Gui();
-        gui.openGUI();
+        if(args[0].equalsIgnoreCase("--headless")){
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    PortChecker portChecker = new PortChecker();
+
+                    for(int i=0; i<65535; i++) {
+                        if (portChecker.isPortUsed("localhost", i)){
+                            System.out.println(i);
+                        }
+                    }
+                }
+            });
+            t.start();
+        }else if(args[0].equalsIgnoreCase("--help")){
+            System.out.println("--help\tZeige die Hilfe an.");
+            System.out.println("--headless\tGibt alle belegten Ports aus ohne eine GUI zu öffnen.");
+        }else {
+            Gui gui = new Gui();
+            gui.openGUI();
+        }
     }
 }
