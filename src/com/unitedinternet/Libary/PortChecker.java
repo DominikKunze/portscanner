33c8dc79c1d9f0a7d44d72f5cf107611c40926ce
DominikKunze <dominik.kunze99@web.de>
2019-12-02 13:02:36 +0100

    Add PortChecker

---
+++ src/com/unitedinternet/Libary/PortChecker.java
@@ -0,0 +1,17 @@
+package com.unitedinternet.Libary;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class PortChecker {
+    public boolean isPortUsed(String host, int port){
+        boolean connected = false;
+        try {
+            Socket socket = new Socket(host,port);
+            socket.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
